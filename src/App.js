76f0bbc01f3aa9205bbb303d7fceaf047bb14439
penksantuyyy <penksantuyyy@gmail.com>
2020-06-18 10:26:43 +0800

    Menu item use withRouter HOC from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const TopicList = (props) => {
+  return (
+    <div>
+      <h1>Topic List</h1>
+    </div>
+  );
+};
+
+const TopicDetail = (props) => {
+  return (
+    <div>
+      <h1>Topic Detail {props.match.params.topicId}</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicList} />
+      <Route path='/topics/:topicId' component={TopicDetail} />
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,29 +12,34 @@ class Directory extends React.Component {
         {
           title: 'Jual Kopi',
           imageUrl: 'https://s-ecom.ottenstatic.com/original/5edfd49da3013093919569.jpg',
-          id: 1
+          id: 1,
+          linkUrl: 'beliKopi'
         },
         {
           title: 'Alat Kopi',
           imageUrl: 'https://s-ecom.ottenstatic.com/original/5edfd4bb3284e375139615.jpg',
-          id: 2
+          id: 2,
+          linkUrl: ''
         },
         {
           title: 'Seduh Kopi',
           imageUrl: 'https://s-ecom.ottenstatic.com/original/5edfd4fde09aa565089632.jpg',
-          id: 3
+          id: 3,
+          linkUrl: ''
         },
         {
           title: 'Bibit Indonesia',
           imageUrl: 'https://s-ecom.ottenstatic.com/original/5edfd5e273296896512649.jpg',
           size: 'large',
-          id: 4
+          id: 4,
+          linkUrl: ''
         },
         {
           title: 'Wholesale',
           imageUrl: 'https://s-ecom.ottenstatic.com/original/5edfd62320b2b680852827.jpg',
           size: 'large',
-          id: 5
+          id: 5,
+          linkUrl: ''
         },
       ]
     }
@@ -44,8 +49,8 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({ title, imageUrl, id, size }) => (
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+          this.state.sections.map(({ id, ...otherSectionProps }) => (
+            <MenuItem key={id} {...otherSectionProps} />
           ))
         }
       </div>
